a2052eaee0c73cdbf09edb5afc128e383ccb93cf
PedroDev <pedrohenriquelouresoliveira@gmail.com>
2023-06-17 14:52:49 -0300

    creat menu

---
+++ index.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+
+using namespace std;
+
+void menu(){
+    
+    cout << "____________________________________________\n" << endl;
+    
+    cout <<"\t      LOJA DO SEU ZE"<<endl;
+    
+    cout << "____________________________________________" << endl;
+    
+    cout <<"0 => Exibir base de dados" << endl;
+    cout <<"1 => Imprimir base de dados" << endl;
+    cout <<"2 => Inserir produto" << endl;
+    cout <<"3 => Procurar pro nome" << endl;
+    cout <<"4 => Listar produtos abaixo do estoque" <<endl;
+    cout <<"5 => Atualizar todos os dados" <<endl;
+    cout <<"6 => Atualizar produto" << endl;
+    cout <<"7 => Comprar produto"<< endl;
+    cout <<"8 => Vender produto" << endl;
+    cout <<"9 => Sair" <<endl;
+}
+
+int main()
+{
+    
+menu();
+    return 0;
+}
