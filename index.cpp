8b8e1fc2288a3d76e3db1f6537fdcd2ce9a7d44c
PedroDev <pedrohenriquelouresoliveira@gmail.com>
2023-06-17 16:42:04 -0300

    função 1 2 0 criadas

--- index.cpp
+++ index.cpp
@@ -3,15 +3,22 @@
 
 using namespace std;
 
+int i = 0;
+
+struct Produto {
+    int id;
+    string nome;
+    float custo;
+    float preco;
+    int estoque;
+    
+};
+
 void menu(){
-    
     cout << "____________________________________________\n" << endl;
-    
     cout <<"\t      LOJA DO SEU ZE"<<endl;
-    
     cout << "____________________________________________" << endl;
-    
-    cout <<"0 => Exibir base de dados" << endl;
+    cout <<"0 => Criar base de dados" << endl;
     cout <<"1 => Imprimir base de dados" << endl;
     cout <<"2 => Inserir produto" << endl;
     cout <<"3 => Procurar pro nome" << endl;
@@ -20,12 +27,71 @@ void menu(){
     cout <<"6 => Atualizar produto" << endl;
     cout <<"7 => Comprar produto"<< endl;
     cout <<"8 => Vender produto" << endl;
-    cout <<"9 => Sair" <<endl;
+    cout <<"9 => Sair\n" <<endl;
+    cout <<"*********************"<<endl;
+    cout <<"Escolha uma opcção:"<<endl;
+    cout <<"*********************"<<endl;
+}
+void imprimirProdutos(Produto *p,int limite_estoque){
+    int contador = 0;
+    while(contador < limite_estoque){
+        if(p[contador].id > 0){
+            cout <<"\nid:"<< p[contador].id << endl;
+            cout <<"nome:"<< p[contador].nome << endl;
+            cout <<"preço:"<< p[contador].preco << endl;
+            cout <<"custo:"<< p[contador].custo << endl;
+            cout <<"estoque:"<< p[contador].nome << endl;
+            contador++;
+        }
+    }
+}
+void inserirProduto(Produto *p){
+    Produto produto;
+    produto.id = i + 1;
+    
+    //criar produto
+    cout <<"Indforme o nome do produto:";
+    cin >> produto.nome;
+    cout <<"Informe o custo do produto:";
+    cin >> produto.custo;
+    cout <<"Informe o valor do produto:";
+    cin >> produto.preco;
+    cout <<"Informe o estoque do produto:";
+    cin >> produto.estoque;
+    //Joga no array
+    
+    p[i] = produto;
+    i++;
+}
+
+int criarBaseDados(){
+    int limite_estoque;
+    cout <<"QUAL LIMITE DO ESTOQUE:";
+    cin >> limite_estoque;
+    
+    return limite_estoque;
 }
 
 int main()
 {
+    Produto  *p;
+    int opcao = 0;
+    int limite_estoque;
+    while(opcao != 9){
+        menu();
     
-menu();
+        cin >> opcao;
+    
+        if (opcao == 0){
+            limite_estoque = criarBaseDados();
+            p = new Produto[limite_estoque];
+        
+        }if(opcao == 1){
+            imprimirProdutos(p,limite_estoque);
+        }
+        if(opcao == 2){
+            inserirProduto(p);
+        }
+    }
     return 0;
 }
